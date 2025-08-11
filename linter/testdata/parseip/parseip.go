1f3c79750fb368f76da43c7c83db9f9e885f9363
Anthony Metzidis <anthony.metzidis@gmail.com>
2025-08-11 16:44:03 -0700

    negative test for parseip

--- linter/testdata/parseip/parseip.go
+++ linter/testdata/parseip/parseip.go
@@ -1,4 +1,4 @@
-package parseip_test
+package main
 
 import (
 	"fmt"
@@ -13,22 +13,20 @@ func main() {
 		fmt.Printf("Invalid IP address: %s\n", ipStr)
 		return
 	}
+	fmt.Println(ip.String())
+}
 
-	// Check if the parsed IP is an IPv4 address.
-	// If it's an IPv6 address, ip.To4() will return nil.
-	// if ip.To4() != nil {
-	// 	fmt.Printf("Parsed IP is IPv4: %s\n", ip.String())
-	// } else if ip.IsLoopback() {
-	// 	fmt.Printf("Parsed IP is IPv6 loopback: %s\n", ip.String())
-	// } else if ip.IsPrivate() {
-	// 	fmt.Printf("Parsed IP is IPv6 private: %s\n", ip.String())
-	// } else if ip.IsGlobalUnicast() {
-	// 	fmt.Printf("Parsed IP is IPv6 global unicast: %s\n", ip.String())
-	// } else {
-	// 	fmt.Printf("Parsed IP is IPv6 (non-IPv4, non-loopback, non-private, non-global unicast): %s\n", ip.String())
-	// }
+func GoodIpv4() {
+	ipStr := "2001:db8::68"
 
-	// net.ParseIP handles both IPv4 and IPv6. To check that it's *not* IPv4,
-	// you can see if the To4() method returns nil.
+	ip := net.ParseIP(ipStr)
+	if ip == nil {
+		fmt.Printf("Invalid IP address: %s\n", ipStr)
+		return
+	}
+
+	if ip.To4() != nil {
+		fmt.Printf("Parsed IP is IPv4: %s\n", ip.String())
+	}
 	fmt.Println(ip.String())
 }
